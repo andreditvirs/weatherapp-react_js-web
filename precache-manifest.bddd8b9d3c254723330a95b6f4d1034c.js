02d37cf6bb93cbc260bcb3f14cfac0d73811bd20
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-09-08 09:54:54 +0700

    Updates

---
+++ precache-manifest.bddd8b9d3c254723330a95b6f4d1034c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3a3d5192f129d904a94b32ae1b66f9",
+    "url": "/weatherapp-react_js-web/index.html"
+  },
+  {
+    "revision": "b4106fb415c048f1e992",
+    "url": "/weatherapp-react_js-web/static/css/2.27fa24ea.chunk.css"
+  },
+  {
+    "revision": "8c63d8643c40e4b67574",
+    "url": "/weatherapp-react_js-web/static/css/main.04ea1fec.chunk.css"
+  },
+  {
+    "revision": "b4106fb415c048f1e992",
+    "url": "/weatherapp-react_js-web/static/js/2.e0cc8ac5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp-react_js-web/static/js/2.e0cc8ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c63d8643c40e4b67574",
+    "url": "/weatherapp-react_js-web/static/js/main.d827884c.chunk.js"
+  },
+  {
+    "revision": "8ec2a7f7006a071f7849",
+    "url": "/weatherapp-react_js-web/static/js/runtime-main.6c66a5b8.js"
+  },
+  {
+    "revision": "203d767a6ec888ead6e4b55b4c24cdc1",
+    "url": "/weatherapp-react_js-web/static/media/background.203d767a.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherapp-react_js-web/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherapp-react_js-web/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherapp-react_js-web/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherapp-react_js-web/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weatherapp-react_js-web/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
